f5a2b37d338e4e8ea1ab8f44e15e884e6d4f0441
Alexgolyd <alexgolyd@rambler.ru>
2020-03-21 15:23:41 +0300

    test

--- src/main/java/ru/iamvidineev/restexample/model/Client.java
+++ src/main/java/ru/iamvidineev/restexample/model/Client.java
@@ -2,12 +2,33 @@ package ru.iamvidineev.restexample.model;
 
 
 public class Client {
+
     private Integer id;
     private String name;
     private String price;
-    private String phone;
 
-    public void setId(int clientId) {
+    public Integer getId() {
+        return id;
     }
-}
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+}

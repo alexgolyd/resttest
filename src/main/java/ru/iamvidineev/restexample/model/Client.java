72a1574207d68a7a8ffaccd46de1abed1a6c0848
Alexgolyd <alexgolyd@rambler.ru>
2020-03-21 13:20:20 +0300

    Initial commit

---
+++ src/main/java/ru/iamvidineev/restexample/model/Client.java
@@ -0,0 +1,13 @@
+package ru.iamvidineev.restexample.model;
+
+
+public class Client {
+    private Integer id;
+    private String name;
+    private String price;
+    private String phone;
+
+    public void setId(int clientId) {
+    }
+}
+
